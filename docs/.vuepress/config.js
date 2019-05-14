e0bf935cc0478ff7afebc6528b49d92a9c7860c0
曾婷 <zengting@peilian.com>
2019-05-14 18:02:30 +0800

    提交测试

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title:'zt',
+    description: '',
+    themeConfig: {
+        nav: [
+            { text: '分享', link: '/share/' }
+        ],
+        siderbar: [
+            {
+                title: '分享',
+                collapsable: false,
+                children: []
+            }
+        ]
+    }
+}
