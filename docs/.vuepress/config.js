f891f2dd929a60ff7fffe2a29ffb777543fb2c30
zzengzeng <2229425959@qq.com>
2019-09-17 15:06:24 +0800

    merge update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,8 @@
 module.exports = {
     title:'zz',
     description: '',
-<<<<<<< HEAD
     dest: './docs/dist',
     base: '/vuepress-demo/',
-=======
-    dest: './dist',
-    base: '/',
->>>>>>> a6bc9a8f26f586e8780121f45f4fb3c169dadd22
     themeConfig: {
         nav: [
             { text: 'vuepress-demo', link: '/share/' }
