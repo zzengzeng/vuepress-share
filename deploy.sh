97f03db72151664b838cab367925bbf7dbe1e972
zzengzeng <2229425959@qq.com>
2019-09-27 17:06:15 +0800

    reset deploy

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # 如果使用 travis 持续集成
-git push -f https://github.com/zzengzeng/vue-admin-template.git master:gh-pages
+git push -f https://github.com/zzengzeng/vuepress-demo.git master:gh-pages
 
 cd -
